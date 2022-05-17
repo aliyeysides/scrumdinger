b78df4d553988c59b897af3547a83dbac0e17756
Ali Yeysides <ali.yeysides@gmail.com>
2022-05-17 14:25:22 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ali Yeysides on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
